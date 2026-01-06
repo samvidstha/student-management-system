e3dbbda4c2a13db1d33f458c2efa054cc3cfb577
samvidstha <ssamvidshrestha@gmail.com>
2026-01-06 16:20:49 +0545

    Added GradeCalculator feature

---
+++ GradeCalculator.java
@@ -0,0 +1,10 @@
+public class GradeCalculator {
+
+    public static double calculateAverage(int[] grades) {
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return (double) sum / grades.length;
+    }
+}
